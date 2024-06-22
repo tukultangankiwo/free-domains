19a1868eb868cd5194e92b1f610479ccbacfa05f
tukultangankiwo <northsevent012@gmail.com>
2024-06-22 09:40:45 +0700

    Create north.1bt.uk.js

---
+++ domains/north.1bt.uk.js
@@ -0,0 +1,10 @@
+addSubDomain({
+  description: 'Personal Blog For Linux Information In Bahasa Indonesia', 
+  domain: '1bt.uk', 
+  subdomain: 'north', 
+  owner: {
+    email: 'northsevent012@gmail.com',
+  },
+  record: {
+    NS: ['ns1.hostry.com', 'ns2.hostry.com', 'ns3.hostry.com', 'ns4.hostry.com'],
+},
